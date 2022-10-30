8b0849c884bf7716e4ebe522c75c3dd20c3f7fab
cute_panda <asdf3601a@gmail.com>
2022-10-30 07:22:47 +0000

    added app_command (TODO)

--- src/main.py
+++ src/main.py
@@ -26,17 +26,51 @@ class CustomBot(discord.Client):
             await self.tree.sync(guild=guild)
         return await super().setup_hook()
 
-    async def on_ready(self):
-        print(f"We have logged in as {self.user}")
-
-    async def on_message(self, msg: discord.Message):
-        print(
-            f"{msg.created_at}[{msg.guild.name}-{msg.channel.name}]{msg.author.name}: {msg.content}"
-        )
-
 
 custom_intents = discord.Intents.default()
 custom_intents.message_content = True  # pylint: disable=assigning-non-slot
 client = CustomBot(intents=custom_intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+
+
+@client.event
+async def on_message(msg: discord.Message):
+    print(
+        f"{msg.created_at}[{msg.guild.name}-{msg.channel.name}]{msg.author.name}: {msg.content}")
+
+
+@client.tree.command()
+async def rss(interaction: discord.Interaction):
+    """顯示已經訂閱的RSS列表"""
+    await interaction.response.send_message("已經訂閱的RSS列表")
+
+
+@client.tree.command()
+@discord.app_commands.describe(
+    rss_url="RSS URL"
+)
+async def sub(interaction: discord.Interaction, rss_url: str):
+    """新訂閱"""
+    await interaction.response.send_message(f"{rss_url} 訂閱成功")
+
+
+@client.tree.command()
+@discord.app_commands.describe(
+    rss_url="RSS URL"
+)
+async def unsub(interaction: discord.Interaction, rss_url: str):
+    """取消訂閱"""
+    await interaction.response.send_message(f"{rss_url} 取消訂閱成功")
+
+
+@client.tree.command()
+async def export(interaction: discord.Interaction):
+    """輸出OPML"""
+    await interaction.response.send_message("TODO")
+
 
 client.run(TOKEN)
