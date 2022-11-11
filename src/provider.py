0caa4e4d6888a287b45d8fdd3bad4401722f0688
cute_panda <asdf3601a@gmail.com>
2022-11-11 07:14:29 +0000

    edge user-agent :D

--- src/provider.py
+++ src/provider.py
@@ -87,7 +87,9 @@ class Feed():
 
 
 async def fetch(url: str, timeout=300):
-    return requests.get(url, timeout=timeout).text
+    return requests.get(url, timeout=timeout, headers={
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"
+    }).text
 
 
 async def parse(xml: str) -> Feed:
