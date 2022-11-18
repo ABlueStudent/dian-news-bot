13cc2bace027f043316689ddc394b4e193c0c34b
cute_panda <asdf3601a@gmail.com>
2022-11-18 11:21:12 +0000

    match format with regex

--- src/provider.py
+++ src/provider.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-class-docstring, missing-module-docstring
 
 import asyncio
+import re
 from time import strptime
 import feedparser
 
@@ -14,10 +15,10 @@ async def fetch_timeout(url: str, timeout=10):
 
 
 def timeparse(time: str):
-    if time.endswith("+0000"):
+    if re.match(r"\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} (\+|\-)\d{4}", time) is not None:
         # Example: Sat, 23 Apr 2022 20:04:56 +0000
         return strptime(time, "%a, %d %b %Y %H:%M:%S %z")
-    if time.endswith("GMT"):
+    if re.match(r"\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} GMT", time) is not None:
         # Example: Wed, 05 Oct 2022 17:08:00 GMT
         return strptime(time, "%a, %d %b %Y %H:%M:%S GMT")
 
