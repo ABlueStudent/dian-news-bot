a99d0fbc373df5369884222af2ba1824431e25e2
cute_panda <asdf3601a@gmail.com>
2022-11-18 11:03:04 +0000

    fixed timeparse method

--- src/provider.py
+++ src/provider.py
@@ -16,9 +16,9 @@ async def fetch_timeout(url: str, timeout=10):
 def timeparse(time: str):
     if time.endswith("+0000"):
         # Example: Sat, 23 Apr 2022 20:04:56 +0000
-        return strptime(time, "%a, %d %b %Y %H:%M:%S +0000")
+        return strptime(time, "%a, %d %b %Y %H:%M:%S %z")
     if time.endswith("GMT"):
         # Example: Wed, 05 Oct 2022 17:08:00 GMT
         return strptime(time, "%a, %d %b %Y %H:%M:%S GMT")
 
-    return ""
+    return
