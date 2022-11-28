851376e1a19549c019d0a5e88f66238118e3fe64
cute_panda <asdf3601a@gmail.com>
2022-11-28 01:14:04 +0000

    what's wrong with me, yesterday.

--- src/provider.py
+++ src/provider.py
@@ -7,16 +7,12 @@ import feedparser
 
 
 async def fetch_rss(url: str):
-    agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"
     feed = feedparser.parse(
         url,
-        agent=agent
+        agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"
     )
     if feed.status in range(300, 400):
-        feed = feedparser.parse(
-            feed.href,
-            agent=agent
-        )
+        return fetch_rss(feed.href)
 
     return feed
 
