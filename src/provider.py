a5a901695e016919ffb173c47c2a5ddc5a5c67ca
cute_panda <asdf3601a@gmail.com>
2022-12-06 11:00:31 +0000

    regex match edited

--- src/provider.py
+++ src/provider.py
@@ -26,10 +26,10 @@ async def fetch_timeout(url: str, timeout=10):
 
 
 def timeparse(time: str):
-    if re.match(r"\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} (\+|\-)\d{4}", time) is not None:
+    if re.match(r"\w{3}, \d{1,2} \w{3} \d{4} \d{2}:\d{2}:\d{2} (\+|\-)\d{4}", time) is not None:
         # Example: Sat, 23 Apr 2022 20:04:56 +0000
         return strptime(time, "%a, %d %b %Y %H:%M:%S %z")
-    if re.match(r"\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}", time) is not None:
+    if re.match(r"\w{3}, \d{1,2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}", time) is not None:
         # Example: Wed, 05 Oct 2022 17:08:00 GMT
         return strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
 
