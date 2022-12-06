e9a3d8ff1bb933671d7cea91152cbf99ee41a100
cute_panda <asdf3601a@gmail.com>
2022-12-06 17:58:28 +0000

    removed unused param

--- src/provider.py
+++ src/provider.py
@@ -6,7 +6,7 @@ from time import strptime
 import feedparser
 
 
-async def fetch_rss(url: str, cooldown=3):
+async def fetch_rss(url: str):
     return feedparser.parse(
         url,
         agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"
